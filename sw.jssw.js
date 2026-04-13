6263ed16a339428aa1c3ce1b82725fa4c1c7cc15
ozenesendemir-cell <ozenesendemir@gmail.com>
2026-04-13 22:03:44 +0300

    Create sw.jssw.js

---
+++ sw.jssw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'byeframe-v1';
+const ASSETS = ['./', './index.html', './style.css', './app.js'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
